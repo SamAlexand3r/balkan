850401ffbdd37c12a8d14164ee302a242732b1ca
Sam Alexander <Samalexunder@yandex.ru>
2024-09-08 20:33:01 +0700

    update 3

--- src/App.js
+++ src/App.js
@@ -3,12 +3,22 @@ import './App.css';
 import Header from './header';
 import Main from './main';
 import backgroundVideo from './assets/background.webm'; 
+import backgroundVideoMP4 from './assets/background.mp4'; // Добавьте альтернативный формат видео
+
 
 function App() {
   return (
     <div className="App">
-      <video autoPlay loop muted className="background-video">
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="background-video"
+      >
+        <source src={backgroundVideoMP4} type="video/mp4" /> 
         <source src={backgroundVideo} type="video/webm" />
+        Ваш браузер не поддерживает воспроизведение видео.
       </video>
       <div className="content">
         <Header />
@@ -17,6 +27,5 @@ function App() {
     </div>
   );
 }
-
 
 export default App;
