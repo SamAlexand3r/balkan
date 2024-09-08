01d72c43dfc2c46201f390082a62981c3082ed98
Sam Alexander <Samalexunder@yandex.ru>
2024-09-09 00:18:15 +0700

    main update

--- src/main.js
+++ src/main.js
@@ -8,30 +8,35 @@ import slovenLogo from './assets/sloven.png';
 import macedLogo from './assets/Mkd.png';
 
 const Main = () => {
-  return (
-    <div className="main">
-      <div className="logo-grid">
-        <a href="http://95.165.24.199/" target="_blank" rel="noopener noreferrer" className="country-box no-hover">
-          <img src={monteLogo} alt="Monte-i" className="country-logo" />
-        </a>
-        <div className="country-box">
-          <img src={serbLogo} alt="Serb-i" className="country-logo" />
+    return (
+        <div className="main">
+            <div className="logo-grid">
+                <a href="https://monte-i.com" target="_blank" rel="noopener noreferrer" className="country-box">
+                    <img src={monteLogo} alt="Monte-i" className="country-logo" />
+                </a>
+                <a href="#" className="country-box coming-soon">
+                    <img src={serbLogo} alt="Serb-i" className="country-logo" />
+                    <div className="coming-soon">Coming Soon</div>
+                </a>
+                <a href="#" className="country-box coming-soon">
+                    <img src={croatLogo} alt="Hrvat-i" className="country-logo" />
+                    <div className="coming-soon">Coming Soon</div>
+                </a>
+                <a href="#" className="country-box coming-soon">
+                    <img src={bosniaLogo} alt="Bosna-i" className="country-logo" />
+                    <div className="coming-soon">Coming Soon</div>
+                </a>
+                <a href="#" className="country-box coming-soon">
+                    <img src={slovenLogo} alt="Sloven-i" className="country-logo" />
+                    <div className="coming-soon">Coming Soon</div>
+                </a>
+                <a href="#" className="country-box coming-soon">
+                    <img src={macedLogo} alt="Mkd-i" className="country-logo" />
+                    <div className="coming-soon">Coming Soon</div>
+                </a>
+            </div>
         </div>
-        <div className="country-box">
-          <img src={croatLogo} alt="Hrvat-i" className="country-logo" />
-        </div>
-        <div className="country-box">
-          <img src={bosniaLogo} alt="Bosna-i" className="country-logo" />
-        </div>
-        <div className="country-box">
-          <img src={slovenLogo} alt="Sloven-i" className="country-logo" />
-        </div>
-        <div className="country-box">
-          <img src={macedLogo} alt="Mkd-i" className="country-logo" />
-        </div>
-      </div>
-    </div>
-  );
-};
+    );
+}
 
 export default Main;
